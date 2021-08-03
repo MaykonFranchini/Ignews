056df16456c2c3f1cf70957252654c435949f034
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-08-03 04:07:52 +0100

    Header component created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import LogoImg from '../../../public/images/logo.svg';
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Image src={LogoImg} alt="ig.news logo" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
