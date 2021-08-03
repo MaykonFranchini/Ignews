702fe271233a3a07a0c239c6f347cb6bedc04136
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-08-03 06:54:33 +0100

    Stripe payment setup done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,32 @@
+import { GetStaticProps } from 'next';
+
 import Head from 'next/head'
 import Image from 'next/image';
 import AvatarImg from '../../public/images/avatar.svg';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
-export default function Home() {
+
+// Client-Side
+// Server-Side
+// Ststic Site Generation
+
+//post do blog 
+
+// Conteudo (SSG - Static Site Generation)
+// Coments (Client-Side)
+
+
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+
   return (
     <>
       <Head>
@@ -15,11 +37,31 @@ export default function Home() {
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>Get access to all the publications <br />
-          <span>for £9.99</span></p>
-          <SubscribeButton />
+          <span>for {product.amount} /month</span></p>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <Image src={AvatarImg} alt="girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JKFpFGgA9C0vCEsNpak5e1P')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-UK', {
+      style: 'currency',
+      currency: 'GBP'
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60*60*24, //24 hours
+  }
+
 }
