ea3f4adad81a6480e4d525b531add3820fe60441
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-11-11 10:08:12 +0000

    test setup completed

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testIgnorePatterns: ["/node_modules/", "/next/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  transform: {
+    "^.+\\.(js|ts|jsx|tsx)$": "<rootDir>/node_modules/babel-jest" 
+  },
+  testEnviroment: "jsdom",
+};
